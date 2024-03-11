7baa4a02c16fe031c99ebbdd933575c7961ac6a6
p32929 <p32929@yahoo.com>
2024-03-11 13:01:52 +0400

    added compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import { compression } from 'vite-plugin-compression2'
+
 
 export default defineConfig({
-  plugins: [react(
-    {
-      tsDecorators: true,
-    }
-  )],
+  plugins: [
+    react(
+      {
+        tsDecorators: true,
+      }
+    ),
+    compression(
+      {
+        algorithm: "brotliCompress"
+      }
+    ),
+  ],
   server: {
     port: 3000,
   },
