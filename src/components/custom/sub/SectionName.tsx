86f005e378d633814a9997648181805cef0ce5a6
p32929 <p32929@yahoo.com>
2024-10-19 08:07:55 +0400

    changed to zinc theme

--- src/components/custom/sub/SectionName.tsx
+++ src/components/custom/sub/SectionName.tsx
@@ -31,7 +31,7 @@ const SectionName: React.FC<Props> = (props) => {
   }
 
   return (
-    <div className="flex flex-col bg-gray-900 mb-2 sticky top-0 z-10 ">
+    <div className="flex flex-col bg-primary-foreground mb-2 sticky top-0 z-10 ">
       <div className="flex flex-row items-center px-4 py-2 justify-between">
         <div className="flex flex-row items-center">
           <Star className="mr-2 h-4 w-4" />
